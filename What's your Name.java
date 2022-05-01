33af45d146a07e955428ce9c38d578d6be50a7d4
Dragon9000UrielDev <104727302+Dragon9000UrielDev@users.noreply.github.com>
2022-05-01 11:53:00 -0500

    Add files via upload

---
+++ What's your Name.java
@@ -0,0 +1,7 @@
+public class MyClass {
+    public static void main(String args[]) {
+      System.out.println("Hello World!");
+         System.out.println("What's your name?:");
+         System.out.print("Bob the Third");
+    }
+}
