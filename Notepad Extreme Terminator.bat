bb6ff4f095dfca4dee74a4ff9ea7e246d94fb920
Dragon9000UrielDev <104727302+Dragon9000UrielDev@users.noreply.github.com>
2022-07-05 13:24:04 -0500

    Add files via upload

---
+++ Notepad Extreme Terminator.bat
@@ -0,0 +1,114 @@
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+TASKKILL /im notepad.exe
+TASKKILL /im notepad.exe
+TASKILL /im notepad.exe
+C:
+D:
+E:
+F:
+H:
+A:
+B:
+TASKILL /im explorer.exe
+TASKILL /im explorer.exe
+start explorer.exe
+exit
